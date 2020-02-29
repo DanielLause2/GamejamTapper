c7d861d3c1422bc24fb59e0edce457874c292398
TorbenZurhelle <t.zurhelle@tiplu.de>
2020-02-29 23:47:43 +0100

    mapControllers

---
+++ Assets/Scripts/Map/DoorController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    private bool RoomConnected = false;
+    public bool Open;
+    public Direction Orientation;
+    public GameObject NextRoom;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Open && other.gameObject.tag == "Player")
+        {
+            if (!RoomConnected)
+            {
+                RoomConnected = true;
+                GameObject room=null;
+                
+                switch (Orientation)
+                {
+                    case Direction.NORTH: 
+                        room = Instantiate(NextRoom, new Vector3(this.transform.position.x, this.transform.position.y + (this.NextRoom.transform.localScale.y/2)), Quaternion.identity);
+                        break;
+                    case Direction.EAST:
+                        room= Instantiate(NextRoom, new Vector3(this.transform.position.x+ (this.NextRoom.transform.localScale.x / 2), this.transform.position.y ), Quaternion.identity);
+                        break;
+                    case Direction.SOUTH:
+                       room= Instantiate(NextRoom, new Vector3(this.transform.position.x, this.transform.position.y - (this.NextRoom.transform.localScale.y / 2)), Quaternion.identity);
+                        break;
+                    case Direction.WEST: 
+                        room=Instantiate(NextRoom, new Vector3(this.transform.position.x - (this.NextRoom.transform.localScale.x / 2), this.transform.position.y), Quaternion.identity);
+                        break;
+                }
+                RoomController roomController = room.GetComponent<RoomController>();
+                roomController.LeaveRoom(Orientation);
+            }
+                 
+             
+        }
+    }
+    
+}
+
+
+  public enum Direction { 
+        NORTH=0,
+        EAST,
+        SOUTH,
+        WEST
+    }
---
+++ Assets/Scripts/Map/RoomController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public List<GameObject> nextRooms;
+    public Direction entrance;
+    public List<DoorController> doors;
+   
+    public bool cleared;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cleared)
+        {
+            RoomClear();
+        }
+        else 
+        {
+            RoomLoockDown();
+        }
+    }
+
+    void RoomClear()
+    {
+        doors.ForEach(x => { x.Open = true; x.NextRoom = nextRooms[0]; });
+    }
+
+    void RoomLoockDown()
+    {
+        if (doors == null)
+        {
+            
+            doors.ForEach(x => x.Open = false);
+            doors[(int)entrance].Open = false;
+        }
+    }
+
+    public void LeaveRoom(Direction pathOrientation)
+    {
+        entrance = (Direction)(((int)pathOrientation + 2)%4);
+        
+    }
+}
